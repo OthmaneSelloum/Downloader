000312d742771bd8422af85603f862f701760d80
OthmaneSelloum <othmaneselloum7@gmail.com>
2025-06-28 18:30:30 +0100

    interface developpment

--- Interface.py
+++ Interface.py
@@ -1,4 +1,17 @@
+from tkinter import filedialog
 import customtkinter as ctk
+import Fonctionnalities as fn
+
+download_path = ""
+
+# Fonction pour choisir un dossier
+def choisir_dossier():
+    global download_path
+    path = filedialog.askdirectory()
+    if path:
+        download_path = path
+        label_path.configure(text=f"Dossier : {path}")
+
 
 # Configuration de l'apparence
 ctk.set_appearance_mode("dark")
@@ -17,12 +30,20 @@ label.pack(pady=10)
 entry = ctk.CTkEntry(app, width=400)
 entry.pack(pady=10)
 
+# Choix du dossier
+btn_choisir = ctk.CTkButton(app, text="Choisir un dossier", command=choisir_dossier)
+btn_choisir.pack(pady=5)
+
+label_path = ctk.CTkLabel(app, text="Dossier : Aucun", font=("Arial", 12))
+label_path.pack(pady=5)
+
+
 # Bouton MP3
-btn_mp3 = ctk.CTkButton(app, text="Télécharger MP3", command=lambda: print("MP3 button clicked"))
+btn_mp3 = ctk.CTkButton(app, text="Télécharger MP3", command=lambda:fn.Download_mp3(entry.get(),download_path))
 btn_mp3.pack(pady=5)
 
 # Bouton MP4
-btn_mp4 = ctk.CTkButton(app, text="Télécharger MP4", command=lambda: print("MP4 button clicked"))
+btn_mp4 = ctk.CTkButton(app, text="Télécharger MP4", command=lambda:fn.Download_mp4(entry.get(),download_path))
 btn_mp4.pack(pady=5)
 
 # Boucle principale
